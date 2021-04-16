958356abdc77c53c8b9f28fa9885b3f1c2f8fcf2
Filip Baotić <filipbaotic@outlook.com>
2021-04-16 21:44:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $(".n1").click(function(e) {
+        $(".n1").toggleClass("active");
+        $(".s1").toggleClass("active");
+        $(".n2").removeClass("active");
+        $(".s2").removeClass("active");
+        $(".n3").removeClass("active");
+        $(".s3").removeClass("active");
+        $(".n4").removeClass("active");
+        $(".s4").removeClass("active");
+    });
+    $(".n2").click(function(e) {
+        $(".n1").removeClass("active");
+        $(".s1").removeClass("active");
+        $(".n2").toggleClass("active");
+        $(".s2").toggleClass("active");
+        $(".n3").removeClass("active");
+        $(".s3").removeClass("active");
+        $(".n4").removeClass("active");
+        $(".s4").removeClass("active");
+    });
+    $(".n3").click(function(e) {
+        $(".n1").removeClass("active");
+        $(".s1").removeClass("active");
+        $(".n2").removeClass("active");
+        $(".s2").removeClass("active");
+        $(".n3").toggleClass("active");
+        $(".s3").toggleClass("active");
+        $(".n4").removeClass("active");
+        $(".s4").removeClass("active");
+    });
+    $(".n4").click(function(e) {
+        $(".n1").removeClass("active");
+        $(".s1").removeClass("active");
+        $(".n2").removeClass("active");
+        $(".s2").removeClass("active");
+        $(".n3").removeClass("active");
+        $(".s3").removeClass("active");
+        $(".n4").toggleClass("active");
+        $(".s4").toggleClass("active");
+    });
+});
